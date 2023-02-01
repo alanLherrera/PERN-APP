361fc5105da3168b77eec95d5dff748f3f5fda66
alanLherrera <108435763+alanLherrera@users.noreply.github.com>
2023-01-31 18:40:38 -0800

    Routes created and Tested

--- server/index.js
+++ server/index.js
@@ -10,19 +10,73 @@ app.use(express.json());
 //ROUTES
 
 //Create
+app.post("/watchlist", async (req, res) => {
+try {
+  const { description } = req.body;
+  const newMovie = await pool.query("INSERT INTO movie (description) VALUES($1) RETURNING *", [description])
 
+  res.json(newMovie.rows[0])
+} catch (err) {
+  console.error(err.message)
+}
+})
 
 // get all
 
+app.get("/watchlist", async (req, res) => {
+  try {
+    const allMovies = await pool.query("SELECT * FROM movie");
+    res.json(allMovies.rows)
+  } catch (err) {
+    console.error(err.message)
+  }
+})
+
 
 //get one
+app.get("/watchlist/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const movie = await pool.query(" SELECT * FROM movie WHERE movie_id = $1",[id]);
+
+
+    res.json(movie.rows[0]);
+  } catch (err) {
+    console.error(err.message)
+  }
+})
 
 
 //update
 
+app.put("/watchlist/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { description } = req.body;
+    const updateMovie = await pool.query("UPDATE movie SET description = $1 WHERE movie_id = $2", [description, id]);
+
+    res.json(" Movie Updated !")
+  } catch (err) {
+    console.error(err.message)
+  }
+})
+
 
 
 //delete
+
+app.delete("/watchlist/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteMovie = await pool.query("DELETE FROM movie WHERE movie_id = $1", [id])
+
+    res.json(" Movie Deleted !")
+
+  } catch (err) {
+    console.error(err.message)
+    
+  }
+})
 
 
 
